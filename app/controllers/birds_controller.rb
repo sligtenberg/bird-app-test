6081afc2c7198f75d9c03a5db85db6d435311305
stephen ligtenberg <bountyhunter24@gmail.com>
2023-07-11 22:56:50 -0400

    show action

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,8 +1,16 @@
 class BirdsController < ApplicationController
+  
     # GET /birds
     def index
       birds = Bird.all
       render json: birds
     end
-  
+
+    def show
+      bird = Bird.find(params[:id])
+      render json: bird
+    rescue ActiveRecord::RecordNotFound
+      render json: "Bird not found", status: :not_found
+    end
+    
 end
